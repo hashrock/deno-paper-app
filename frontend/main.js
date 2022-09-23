ed5904fd3296ce96e29de75b7822a9ee32f2639b
hashrock <hashrock@users.noreply.github.com>
2022-09-24 08:05:38 +0900

    fmt

--- frontend/main.js
+++ frontend/main.js
@@ -1,46 +1,46 @@
-const canv = document.querySelector('#canv')
-const ctx = canv.getContext('2d', {
+const canv = document.querySelector("#canv");
+const ctx = canv.getContext("2d", {
   desynchronized: true,
-})
-ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)'
-ctx.lineWidth = 3
+});
+ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
+ctx.lineWidth = 3;
 
-let drag = null
+let drag = null;
 
 document.addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
-    ctx.clearRect(0, 0, canv.width, canv.height)
+    ctx.clearRect(0, 0, canv.width, canv.height);
   }
-})
+});
 
-canv.addEventListener('pointerdown', (e) => {
-  canv.setPointerCapture(e.pointerId)
+canv.addEventListener("pointerdown", (e) => {
+  canv.setPointerCapture(e.pointerId);
 
-  const x = e.offsetX
-  const y = e.offsetY
-  drag = { x, y }
-})
+  const x = e.offsetX;
+  const y = e.offsetY;
+  drag = { x, y };
+});
 
-canv.addEventListener('pointermove', (e) => {
-  e.preventDefault()
+canv.addEventListener("pointermove", (e) => {
+  e.preventDefault();
   if (drag) {
-    const x = e.offsetX
-    const y = e.offsetY
-    ctx.beginPath()
-    ctx.moveTo(drag.x, drag.y)
-    ctx.lineTo(x, y)
-    ctx.stroke()
-    drag = { x, y }
+    const x = e.offsetX;
+    const y = e.offsetY;
+    ctx.beginPath();
+    ctx.moveTo(drag.x, drag.y);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    drag = { x, y };
   }
-})
+});
 
-canv.addEventListener('pointerup', (e) => {
-  drag = null
-})
-canv.addEventListener('touchmove', (e) => {
-  e.preventDefault()
-})
+canv.addEventListener("pointerup", (e) => {
+  drag = null;
+});
+canv.addEventListener("touchmove", (e) => {
+  e.preventDefault();
+});
 
-canv.addEventListener('pointercancel', (e) => {
-  drag = null
-})
+canv.addEventListener("pointercancel", (e) => {
+  drag = null;
+});
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Webview, SizeHint } from "https://deno.land/x/webview/mod.ts";
+import { SizeHint, Webview } from "https://deno.land/x/webview/mod.ts";
 import {
   DOMParser,
   Element,
@@ -8,8 +8,8 @@ function bundle() {
   const htmlFile = Deno.readTextFileSync("./frontend/index.html");
 
   const doc = new DOMParser().parseFromString(htmlFile, "text/html")!;
-  const scripts = doc.querySelectorAll("script")
-  
+  const scripts = doc.querySelectorAll("script");
+
   for (const script of scripts) {
     if (script instanceof Element) {
       const el = script as Element;
@@ -22,7 +22,7 @@ function bundle() {
       }
     }
   }
-  
+
   const styles = doc.querySelectorAll("link[rel=stylesheet]");
   for (const style of styles) {
     if (style instanceof Element) {
@@ -37,13 +37,15 @@ function bundle() {
     }
   }
 
-  return doc.querySelector("html")?.outerHTML!
+  return doc.querySelector("html")?.outerHTML!;
 }
 
-
-
 const html = bundle();
-const webview = new Webview(true, {width: 600, height: 600, hint: SizeHint.FIXED});
-webview.title = "Paper"
+const webview = new Webview(true, {
+  width: 600,
+  height: 600,
+  hint: SizeHint.FIXED,
+});
+webview.title = "Paper";
 webview.navigate(`data:text/html,${encodeURIComponent(html)}`);
 webview.run();
